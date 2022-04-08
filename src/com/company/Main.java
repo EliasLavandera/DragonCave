b26396c203bbb1ab0063a332fd6eb1f46637bb30
EliasLavandera <102991754+EliasLavandera@users.noreply.github.com>
2022-04-08 07:51:14 -0700

    Second Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,22 +12,25 @@ public class Main {
         System.out.println("is greedy and hungry and will eat you on sight.");
         System.out.println("Which cave will you go into? (1 or 2)");
 
-        int PlayerChoice = Integer.valueOf(UserInput.nextLine());
-
-        if(PlayerChoice == 1) {
-            System.out.println("You approach the cave...");
-            System.out.println("It is dark and spooky...");
-            System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
-            System.out.println("Gobbles you down in one bite!");
+        //int PlayerChoice = Integer.valueOf(UserInput.nextLine());
+        try {
+            int answer = Integer.parseInt(UserInput.nextLine());
+            if (answer == 1) {
+                System.out.println("You approach the cave...");
+                System.out.println("It is dark and spooky...");
+                System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
+                System.out.println("Gobbles you down in one bite!");
+            } else if (answer == 2) {
+                System.out.println("You approach the cave...");
+                System.out.println("It is dark and spooky...");
+                System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
+                System.out.println("Asks if you would like some tea!");
+            } else {
+                System.out.println("Invalid choice");
+            }
         }
-        else if(PlayerChoice == 2) {
-            System.out.println("You approach the cave...");
-            System.out.println("It is dark and spooky...");
-            System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
-            System.out.println("Asks if you would like some tea!");
-        }
-        else {
-            System.out.println("Invalid choice");
+        catch (NumberFormatException e) {
+            System.out.println("Only cave 1 or 2.");
         }
 
     }
